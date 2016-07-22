74c5a5270c1bf97a6011a12bbde4a6b0bfc13506
theqaangel <angel.tsvetkov@hotmail.com>
2016-07-22 13:15:26 +0300

    Add ranging frequency settings

--- module.js
+++ module.js
@@ -1,1 +1,10 @@
 angular.module('BONI', ['ui.bootstrap', 'ngCookies', 'ngMaterial','md.data.table']);
+
+window.onscroll = function(ev) {
+    if(window.scrollY  > 300){
+        $(".quick-start").css("visibility", "visible").fadeIn();
+    }
+    else {
+        $(".quick-start").fadeOut().css("visibility", "visible");
+    }
+};
